9d489f42003addf771047b38505e0ac962317ea4
Peter Georgopoulos <peter.georgopoulos@technologent.com>
2020-01-30 22:30:22 -0600

    progressing

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # survey-site
-Azure Kubernetes Service/ Docker/ Limesurvey/ MairaDB
+Terraform/ Azure Kubernetes Service/ Docker/ Limesurvey/ MairaDB
+
+My environment:
+Terraform v0.12.16
--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ variable "rg" {
   type = "string"
 }
 
+variable "acr_name" {
+  type = "string"
+}
+
 provider "azurerm" {
   version = "1.38.0"
   subscription_id = var.sub_id
@@ -14,4 +18,14 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = var.rg
   location = "East US 2"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                     = var.acr_name
+  resource_group_name      = "${azurerm_resource_group.rg.name}"
+  location                 = "${azurerm_resource_group.rg.location}"
+  sku                      = "Basic"
+  admin_enabled            = false
+#   The below param is only available with the "Premium" sku.
+#   georeplication_locations = ["West US", "East US"]
 }
