174304da6cf0fb1960f3d8909aa921febe0f013e
Peter Georgopoulos <peter.georgopoulos@technologent.com>
2020-01-30 23:20:10 -0600

    more progress and organization

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-variable "sub_id" {
-  type = "string"
-}
-
-variable "rg" {
-  type = "string"
-}
-
-variable "acr_name" {
-  type = "string"
-}
 
 provider "azurerm" {
   version = "1.38.0"
@@ -22,10 +11,32 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_container_registry" "acr" {
   name                     = var.acr_name
-  resource_group_name      = "${azurerm_resource_group.rg.name}"
-  location                 = "${azurerm_resource_group.rg.location}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   sku                      = "Basic"
   admin_enabled            = false
 #   The below param is only available with the "Premium" sku.
 #   georeplication_locations = ["West US", "East US"]
+}
+
+resource "azurerm_kubernetes_cluster" "kube_clust" {
+  name                = var.kube_clust_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = var.kube_clust_name
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS1_v2"
+  }
+
+  service_principal {
+    client_id     = var.app_id
+    client_secret = var.app_secret
+  }
+
+  tags = {
+    Environment = "limesurvey"
+  }
 }
